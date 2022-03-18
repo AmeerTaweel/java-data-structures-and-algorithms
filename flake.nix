594d486090ffcd15b34aa712a1f68e4c1cf50bf5
Ameer Taweel <ameertaweel2002@gmail.com>
2022-03-18 23:44:37 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+	description = "Java Data Structures And Algorithms";
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+	inputs.flake-utils.url = "github:numtide/flake-utils";
+
+	outputs = { self, nixpkgs, flake-utils }:
+	flake-utils.lib.eachDefaultSystem (system: let
+		pkgs = nixpkgs.legacyPackages.${system};
+	in {
+		devShell = pkgs.mkShell {
+			nativeBuildInputs = with pkgs; [
+				# Dependencies go here
+				jdk
+			];
+			buildInputs = [ ];
+		};
+	});
+}
